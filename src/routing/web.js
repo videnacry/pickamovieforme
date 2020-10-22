e518f350a119ea25c7f63f58912e5586e64d8bec
Ezequiel <ezemgaray@gmail.com>
2020-10-22 17:32:14 +0200

    Update routing/web.js - require review

--- src/routing/web.js
+++ src/routing/web.js
@@ -1,11 +1,13 @@
 // Import routes
 const user = require('./user')
+const review = require('./review')
 
 /**
  * Add routes to app
  */
 const Router = app => {
   user(app)
+  review(app)
 }
 
 module.exports = Router
