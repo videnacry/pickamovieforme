c84e6b4ec81eabe8e1f132d5ca77289e3f48b1e6
Ezequiel <ezemgaray@gmail.com>
2020-10-22 18:17:59 +0200

    Update Router - Adding login , register routes

--- src/routing/web.js
+++ src/routing/web.js
@@ -1,4 +1,6 @@
 // Import routes
+const login = require('./login')
+const register = require('./register')
 const user = require('./user')
 const review = require('./review')
 const comment = require('./comment')
@@ -7,6 +9,8 @@ const comment = require('./comment')
  * Add routes to app
  */
 const Router = app => {
+  login(app)
+  register(app)
   user(app)
   review(app)
   comment(app)
