7c6391d898c00eb0a3f38c1cc9269c4d739891f4
Ezequiel <ezemgaray@gmail.com>
2020-10-25 01:24:20 +0200

    Add User Model

---
+++ src/models/User.js
@@ -0,0 +1,36 @@
+const {
+  DataTypes,
+  Model
+} = require('sequelize')
+const sequelize = require('../database/db')
+const Review    = require('./Review')
+
+class User extends Model {}
+
+
+module.exports = User.init({
+  user_id: {
+    type:           DataTypes.INTEGER,
+    allowNull:      false,
+    primaryKey:     true,
+    autoIncrement:  true
+  },
+  photo:        DataTypes.STRING,
+  description:  DataTypes.STRING,
+  name:         DataTypes.STRING,
+  username:     DataTypes.STRING,
+  email:        DataTypes.STRING,
+  password:     DataTypes.STRING
+},
+{
+  sequelize,
+  modelName:  'user',
+  tableName:  'users',
+  user_id:    'user_id',
+  createdAt:  'creation_date',
+  updatedAt:  'updated_date',
+  deletedAt:  'deleted_date',
+  dialect:    'mysql'
+})
+
+User.hasMany(Review, {foreignKey: 'user_id'})
