9c0c4cdcce9de6540024e24ba663230fe3328e19
Ezequiel <ezemgaray@gmail.com>
2020-10-25 01:52:12 +0200

    Update User Migration

--- src/database/migrations/20201023061008-create_user.js
+++ src/database/migrations/20201023061008-create_user.js
@@ -8,6 +8,56 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    return queryInterface.createTable('users', {
+      user_id: {
+        type:           Sequelize.INTEGER,
+        primaryKey:     true,
+        autoIncrement:  true,
+        allowNull:      false
+      },
+      name: {
+        type:         Sequelize.STRING(50),
+        allowNull:    false
+      },
+      username: {
+        type:         Sequelize.STRING(20),
+        allowNull:    false,
+        unique:       true
+      },
+      email: {
+        type:         Sequelize.STRING(70),
+        allowNull:    false,
+        unique:       true
+      },
+      password: {
+        type:         Sequelize.STRING,
+        allowNull:    false
+      },
+      photo: {
+        type:         Sequelize.STRING,
+        allowNull:    true,
+        defaultValue: "src/assets/profileImage/profile-picture.jpg"
+      },
+      description: {
+        type:         Sequelize.TEXT,
+        allowNull:    true
+      },
+      creation_date: {
+        type:         Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+        allowNull:    false
+      },
+      updated_date: {
+        type:         Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+        allowNull:    false
+      },
+      deleted_date: {
+        type:         Sequelize.DATE,
+        allowNull:    true
+      }
+    })
+
   },
 
   down: async (queryInterface, Sequelize) => {
@@ -17,5 +67,6 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
+    return queryInterface.dropTable('users')
   }
 };
