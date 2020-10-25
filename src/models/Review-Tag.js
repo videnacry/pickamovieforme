88f8d3a41163aa9ee2b3aaefbdf338880429366b
Ezequiel <ezemgaray@gmail.com>
2020-10-25 07:18:14 +0100

    Add ReviewTag Model

---
+++ src/models/Review-Tag.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  DataTypes,
+  Model
+} = require('sequelize');
+const sequelize = require('../database/db')
+
+
+class ReviewTag extends Model {};
+
+module.exports = ReviewTag.init({
+  review_id: {
+    type:       DataTypes.INTEGER,
+    primaryKey: true
+  },
+  tag_id: {
+    type:       DataTypes.INTEGER,
+    primaryKey: true
+  }
+}, {
+  sequelize,
+  modelName: 'reviewtag',
+  tableName: 'review_tag',
+});
+
