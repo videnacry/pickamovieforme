eaaf91b4f11af8e34022b59189a563603d9e0a20
Ezequiel <ezemgaray@gmail.com>
2020-10-25 07:26:27 +0100

    Add Models.js - require Models and export them

---
+++ src/models/Models.js
@@ -0,0 +1,9 @@
+module.exports = {
+  User:               require('./User'),
+  Review:             require('./Review'),
+  Comment:            require('./Comment'),
+  Tag:                require('./Tag'),
+  ReviewTag:          require('./Review-Tag'),
+  UserReviewFavorite: require('./User-Review-Favorite'),
+  UserTagHidden:      require('./User-Tag-Hidden')
+}
