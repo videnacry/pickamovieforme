3abc78141ebcd54493229937c278841fa72539aa
Ezequiel <ezemgaray@gmail.com>
2020-10-25 02:47:40 +0200

    Update Tag Model - Change tableName for 'tags'

--- src/models/Tag.js
+++ src/models/Tag.js
@@ -2,33 +2,23 @@
 const {
   Model
 } = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Tag extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Tag.init({
-    tag_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    }
-  }, {
-    sequelize,
-    modelName: 'Tag',
-    tableName: 'tag',
-    timestamps: false,
-    dialect: 'mysql'
-  });
-  return Tag;
-};
+
+class Tag extends Model {};
+
+module.exports = Tag.init({
+  tag_id: {
+    type:           DataTypes.INTEGER,
+    primaryKey:     true,
+    autoIncrement:  true
+  },
+  name: {
+    type:         DataTypes.STRING,
+    allowNull:    false
+  }
+}, {
+  sequelize,
+  modelName:  'tag',
+  tableName:  'tags',
+  timestamps: false,
+  dialect:    'mysql'
+});
