1ddd0508ddf7d5cad16a5cf03100f833df74bff4
beron <bleink@outlook.es>
2020-10-26 11:01:45 +0100

    Tag model - added validation, not empty, unique name

--- src/models/Tag.js
+++ src/models/Tag.js
@@ -6,21 +6,37 @@ const {
 const sequelize = require('../database/db')
 
 
-class Tag extends Model {};
+class Tag extends Model { };
 
 module.exports = Tag.init({
   tag_id: {
-    type:           DataTypes.INTEGER,
-    primaryKey:     true,
-    autoIncrement:  true
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
   },
   name: {
-    type: DataTypes.STRING
+    type: DataTypes.STRING,
+    validate: {
+      notEmpty: {
+        msg: 'Tag name is required !'
+      },
+      fn: async function (val) {
+
+        const tag = await Tag.findOne({
+          where: {
+            name: val
+          }
+        })
+        if (tag) {
+          throw new Error('Name already exists !')
+        }
+      }
+    }
   }
 }, {
   sequelize,
-  modelName:  'tag',
-  tableName:  'tags',
+  modelName: 'tag',
+  tableName: 'tags',
   timestamps: false,
-  dialect:    'mysql'
+  dialect: 'mysql'
 });
