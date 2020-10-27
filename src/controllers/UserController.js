b67aeff3e9ab7c8453e825ccc0e6361c57a973dd
Ezequiel <ezemgaray@gmail.com>
2020-10-27 21:05:50 +0100

    Refactor UserController - Add storeUser()

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -36,3 +36,71 @@ getUsers = async (req, res) =>{
       })
     })
 }
+
+
+/**
+ * Check if already exist an user with this email
+ */
+emailInUse = async (email) => {
+  const user = await User.findOne({
+    where: {
+      email: email
+    }
+  })
+  if (user) {
+    return {
+      message:  'Email already in user',
+      type:     'Validation error',
+      path:     'email',
+      value:    email,
+    }
+  }
+}
+
+/**
+ * Check if already exist an user with this username
+ */
+usernameInUse = async (username) => {
+  const user = await User.findOne({
+    where: {
+      username: username
+    }
+  })
+  if (user) {
+    return {
+      message: 'Username already in user',
+      type: 'Validation error',
+      path: 'username',
+      value: username
+    }
+  }
+}
+
+/**
+ * Store User.
+ */
+storeUser = async (req, res) => {
+  const user = User.build(req.body)
+  let err = await validationMessage(user)
+  if(!err)
+    err = []
+
+  const mailExist = await emailInUse(req.body.email)
+  const usernameExist = await usernameInUse(req.body.username)
+
+  if(mailExist)
+    err.push(mailExist)
+  if(usernameExist)
+    err.push(usernameExist)
+
+  if (err.length) {
+    res.status(422).json(err)
+  } else {
+    user.save()
+      .then(() => {
+        res.status(200).json({
+          success: true
+        })
+      })
+  }
+}
