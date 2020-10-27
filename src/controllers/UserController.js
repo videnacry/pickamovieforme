c99f405eb9a78b924aa870c22055e76eee72d51d
Ezequiel <ezemgaray@gmail.com>
2020-10-27 21:04:48 +0100

    Refactor UserController  - Add getUsers()

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,23 +1,38 @@
-const Models = require('../models/Models')
-const validationMessage = require('./validationMessage')
+const {User}              = require('../models/Models')
+const validationMessage   = require('./validationMessage')
 
 
-module.exports = {
-
-  storeUser: async (req, res) => {
-
-    const user = Models.User.build(req)
-    const err = await validationMessage(user)
-    if(err){
-      console.log(err);
-      // res.status(422).send(err)
-    }else{
-      // Models.User.create(user)
-      user.save()
-        .then(result =>{
-          console.log('/login', result);
-          // res.status(200).redirect('/login')
-        })
+getUsers = async (req, res) =>{
+  let email = false
+  if(Object.keys(req.query).length){
+    if(req.query.email)
+      email = req.query.email 
+    else{
+      res.status(400).json({
+        success: false,
+        message: 'Bad request'
+      })
+      return
     }
   }
+
+  User.findAll({
+    where: {
+      email: email
+    }
+  })
+    .then(users =>{
+      users.length
+      ? res.status(200).json(users)
+      : res.status(422).json({
+          success: false,
+          message: "User not fount"
+      })
+    })
+    .catch(()=>{
+      res.status(500).json({
+        success: false,
+        message: "The request could not be processed"
+      })
+    })
 }
