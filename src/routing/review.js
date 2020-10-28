a3a00231c83bdc74d38efa022fcbc7d3833a918b
beron <bleink@outlook.es>
2020-10-28 15:56:26 +0100

    review routing - controller added

--- src/routing/review.js
+++ src/routing/review.js
@@ -1,31 +1,31 @@
+const ReviewController = require('../controllers/ReviewController')
+
 /**
  * Define routes of Review's data
  */
 const review = app => {
   // Store review
-  app.post('/reviews', function(req, res){
-  })
+  app.post('/reviews', ReviewController.storeReview)
+
+  app.get('/reviews/search/:title_text', ReviewController.searchMovie)
 
   // Get a single review
-  app.get('/reviews/:review_id', function(req, res){
-  })
+  app.get('/reviews/:review_id', ReviewController.getReviewById)
 
   // Update review
-  app.put('/reviews/:review_id', function(req, res){
-  })
+  app.put('/reviews/:review_id', ReviewController.updateReview)
 
   // Delete review
   app.delete('/reviews/:review_id', function(req, res){
   })
 
   //GET Reviews
-  app.get('/reviews', function(req, res){
-  })
+  app.get('/reviews', ReviewController.getReviews)
 
   // Get comment's reviews
   app.get('/reviews/:review_id/comments', function(req, res){
   })
 
 }
-
+// ReviewController.getReviews({query:{title:'tes'}},{})
 module.exports = review
